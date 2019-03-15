331a65550c11247090632ecf89fd4f415fd1abcd
OJPARKINSON <olliepar01@gmail.com>
2019-03-15 22:11:47 +0000

    Inital comment

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+import Header from "../components/header";
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`Grace Jane`, `application`, `react`]} />
+    <Header />
+
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage

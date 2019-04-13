0ac030497778a3aa30ce85b8e2a19a8d49530889
OJPARKINSON <olliepar01@gmail.com>
2019-04-13 03:25:35 +0100

    no vids and gigs container updated, parallax added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,16 @@ import SEO from "../components/seo";
 import Header from "../components/header";
 import Gig from "../components/gig";
 import Toolbar from "../components/toolbar";
-import ReactPlayer from "react-player";
 
 class IndexPage extends Component {
   state = {
    navOpen: false
   };
-
   drawerToggleClickHandler = () => {
     this.setState((prevState) => {
       return {navOpen: !prevState.navOpen};
     });
   };
-
   render() {
     let nav;
     if (this.state.navOpen) {
@@ -41,14 +38,10 @@ class IndexPage extends Component {
         <div className="buttonContainer">
           <a target="_blank" rel="noopener noreferrer" href="https://ampl.ink/gy1X8" className="fancy-btn">Listen here</a> <br/>
         </div>
-        <div className="parallaxSpacer">
-        </div>
+        <div className="parallaxSpacer"></div>
         <h2 className="subTitle">Upcoming Gigs</h2>
         <div className="gigSetup">
-        {/* Use an svg/gif/png wave or ocean and make it into a favicon as well */}
-          <ReactPlayer className="gigImg" src={require('../images/waves2.gif')} playing />
           <div className="gigsContainer">
-          <h3 id="gigs">Live Shows</h3>
           <Gig dateNum="03" weekDay="MON" month="JAN" location="Crash Records - Leeds, UK" />
           <Gig dateNum="12" weekDay="THUR" month="APR" location="Church - Leeds, UK" />
           <Gig dateNum="10" weekDay="SAT" month="MAY" location="O2 Academy - Leeds, UK" />
